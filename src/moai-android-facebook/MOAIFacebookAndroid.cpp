77881b754562b7e6241b6626fdd2e6ae4566cd25
Андрей Барков <deagle318rus@gmail.com>
2016-07-27 15:01:01 +0300

    fb android bindings fix: method gameSendRequest added params - title, data

--- src/moai-android-facebook/MOAIFacebookAndroid.cpp
+++ src/moai-android-facebook/MOAIFacebookAndroid.cpp
@@ -280,6 +280,8 @@ int MOAIFacebookAndroid::_sendGameRequest ( lua_State* L ) {
 	
 	bool result = self->CallStaticBooleanMethod (
 		self->mJava_SendGameRequest,
+		( jstring )jdata,
+		( jstring )jtitle,
 		( jstring )jmessage,
 		( jstring )jactionType,
 		( jstring )jobjectID,
@@ -454,7 +456,7 @@ MOAIFacebookAndroid::MOAIFacebookAndroid () {
 	this->mJava_PostToFeed					= this->GetStaticMethod ( "postToFeed", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V" );
 	this->mJava_RequestPublishPermissions	= this->GetStaticMethod ( "requestPublishPermissions", "([Ljava/lang/String;)V" );
 	this->mJava_RequestReadPermissions		= this->GetStaticMethod ( "requestReadPermissions", "([Ljava/lang/String;)V" );
-	this->mJava_SendGameRequest				= this->GetStaticMethod ( "sendGameRequest", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;[Ljava/lang/String;[Ljava/lang/String;I)Z" );
+	this->mJava_SendGameRequest				= this->GetStaticMethod ( "sendGameRequest", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;[Ljava/lang/String;[Ljava/lang/String;I)Z" );
 	this->mJava_SessionValid				= this->GetStaticMethod ( "sessionValid", "()Z" );
 	this->mJava_ShowInviteDialog			= this->GetStaticMethod ( "showInviteDialog", "(Ljava/lang/String;Ljava/lang/String;)V" );
 
